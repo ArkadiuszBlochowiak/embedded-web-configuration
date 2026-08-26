3f64d8248219cc7c02e02d72284179f0c51ef45e
Arkadiusz Błochowiak <43909437+ArkadiuszBlochowiak@users.noreply.github.com>
2026-08-26 14:20:17 +0200

    add route config with auth guard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { Login } from './core/components/login/login';
+import { authGuard } from './core/guards/auth-guard';
+import { Home } from './features/home/home';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: Home,
+    canActivate: [authGuard],
+  },
+  {
+    path: 'login',
+    component: Login,
+  },
+];
