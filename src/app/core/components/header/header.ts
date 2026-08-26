ea7aebfe33661ef08bbf507271443f8477625930
Arkadiusz Błochowiak <43909437+ArkadiuszBlochowiak@users.noreply.github.com>
2026-08-26 15:57:24 +0200

    adjust header content

--- src/app/core/components/header/header.ts
+++ src/app/core/components/header/header.ts
@@ -1,9 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { MatIcon } from '@angular/material/icon';
+import { MatToolbar } from '@angular/material/toolbar';
+import { isActive, Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
+import { MatButton } from '@angular/material/button';
 
 @Component({
-  imports: [],
+  imports: [MatToolbar, MatIcon, MatButton],
   selector: 'app-header',
   styleUrl: './header.css',
   templateUrl: './header.html',
 })
-export class Header {}
+export class Header {
+  router = inject(Router);
+  auth = inject(AuthService);
+
+  isLoginPageActive = isActive('/login', this.router);
+}
