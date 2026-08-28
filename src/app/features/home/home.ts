65e453422e4a93169e36d47d71ba51ba4c2fd438
Arkadiusz Błochowiak <43909437+ArkadiuszBlochowiak@users.noreply.github.com>
2026-08-28 12:39:02 +0200

    show loading state for fetching data

--- src/app/features/home/home.ts
+++ src/app/features/home/home.ts
@@ -2,22 +2,35 @@ import { Component, inject, signal } from '@angular/core';
 import { SettingsForm } from './components/settings-form/settings-form';
 import { SettingsService } from './services/settings.service';
 import { SettingsData } from './settings-info';
+import { MatProgressSpinner } from '@angular/material/progress-spinner';
 
 @Component({
-  imports: [SettingsForm],
+  imports: [SettingsForm, MatProgressSpinner],
   selector: 'app-home',
   styleUrl: './home.css',
   // templateUrl: './home.html',
-  template: ` <app-settings-form [initialSettings]="initialSettings()" /> `,
+  template: `
+    @if (isLoading()) {
+      <div class="home-loading">
+        <mat-progress-spinner mode="indeterminate" />
+      </div>
+    } @else {
+      <app-settings-form [initialSettings]="initialSettings()" />
+    }
+
+    <!-- {{ initialSettings() }} -->
+  `,
 })
 export class Home {
   settingsService = inject(SettingsService);
   initialSettings = signal<SettingsData | null>(null);
+  isLoading = signal(false);
 
   constructor() {
+    this.isLoading.set(true);
     this.settingsService.getSettings().then((data: SettingsData) => {
       this.initialSettings.set(data);
-
+      this.isLoading.set(false);
       console.log(this.initialSettings());
     });
   }
--- src/app/features/home/services/settings.service.ts
+++ src/app/features/home/services/settings.service.ts
@@ -5,7 +5,15 @@ import { SettingsData } from '../settings-info';
 export class SettingsService {
   protected url = 'http://localhost:3000/settings';
 
+  async delay() {
+    return new Promise((resolve) => {
+      setTimeout(() => resolve(''), 1000);
+    });
+  }
+
   async getSettings(): Promise<SettingsData> {
+    await this.delay();
+
     const data = await fetch(this.url);
     return (await data.json()) || {};
   }
