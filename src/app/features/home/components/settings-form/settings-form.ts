424060936ae8a19c596b5d46b279ea25d85325af
Arkadiusz Błochowiak <43909437+ArkadiuszBlochowiak@users.noreply.github.com>
2026-08-28 11:41:03 +0200

    fill form with fetched data

--- src/app/features/home/components/settings-form/settings-form.ts
+++ src/app/features/home/components/settings-form/settings-form.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, inject, input, model, signal, SimpleChanges } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatButton } from '@angular/material/button';
 import { MatFormField, MatLabel } from '@angular/material/form-field';
 import { MatSelect, MatOption } from '@angular/material/select';
 import { FormInput } from '../../../../shared/components/form-input/form-input';
 import { ipAddressValidator, submaskValidator } from '../../services/settings.validator';
+import { SettingsService } from '../../services/settings.service';
+import { SettingsData } from '../../settings-info';
 
 @Component({
   imports: [
@@ -21,6 +23,9 @@ import { ipAddressValidator, submaskValidator } from '../../services/settings.va
   templateUrl: './settings-form.html',
 })
 export class SettingsForm {
+  settingsService = inject(SettingsService);
+  initialSettings = input<SettingsData | null>(null);
+
   settingsForm = new FormGroup({
     deviceName: new FormControl('', {
       nonNullable: true,
@@ -29,8 +34,19 @@ export class SettingsForm {
     ipAddress: new FormControl('', [Validators.required, ipAddressValidator]),
     subnetMask: new FormControl('', [Validators.required, submaskValidator]),
     gateway: new FormControl('', [Validators.required, ipAddressValidator]),
-    dhcpMode: new FormControl(''),
+    dhcpMode: new FormControl(false),
   });
+
+  ngOnChanges(changes: SimpleChanges<SettingsForm>) {
+    if (changes.initialSettings && changes.initialSettings !== null) {
+      const init = changes.initialSettings.currentValue;
+      this.settingsForm.controls.deviceName.setValue(init?.deviceName || '');
+      this.settingsForm.controls.ipAddress.setValue(init?.ipAddress || '');
+      this.settingsForm.controls.subnetMask.setValue(init?.subnetMask || '');
+      this.settingsForm.controls.gateway.setValue(init?.gateway || '');
+      this.settingsForm.controls.dhcpMode.setValue(Boolean(init?.dhcpMode) || false);
+    }
+  }
 
   onSubmit() {
     console.log(this.settingsForm.value);
--- src/app/features/home/home.ts
+++ src/app/features/home/home.ts
@@ -1,11 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { SettingsForm } from './components/settings-form/settings-form';
+import { SettingsService } from './services/settings.service';
+import { SettingsData } from './settings-info';
 
 @Component({
   imports: [SettingsForm],
   selector: 'app-home',
   styleUrl: './home.css',
   // templateUrl: './home.html',
-  template: ` <app-settings-form /> `,
+  template: ` <app-settings-form [initialSettings]="initialSettings()" /> `,
 })
-export class Home {}
+export class Home {
+  settingsService = inject(SettingsService);
+  initialSettings = signal<SettingsData | null>(null);
+
+  constructor() {
+    this.settingsService.getSettings().then((data: SettingsData) => {
+      this.initialSettings.set(data);
+
+      console.log(this.initialSettings());
+    });
+  }
+}
